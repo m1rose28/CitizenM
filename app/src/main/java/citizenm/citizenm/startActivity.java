0694a133fbf56847bf0ca95ec7b49454c4bc362e
markrose <m1rose28@gmail.com>
2016-12-27 14:17:44 -0800

    Added json class for communicating

---
+++ app/src/main/java/citizenm/citizenm/startActivity.java
@@ -0,0 +1,50 @@
+package citizenm.citizenm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+public class startActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        final TextView mTxtDisplay;
+        ImageView mImageView;
+
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+        mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+        String url = "http://104.236.191.175/citizenm/data.php";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTxtDisplay.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        AppController.getInstance().addToRequestQueue(jsObjRequest);
+
+
+    }
+}
